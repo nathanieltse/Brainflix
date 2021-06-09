7d63ae5e6f93fcc163308ef9d25838bde0371efe
nathanieltse <nathanieltse@hotmail.com>
2021-06-09 01:11:57 -0400

    added breakpoint for nav bar

--- sprint-one/src/components/Nav/SearchBar/SearchBar.jsx
+++ sprint-one/src/components/Nav/SearchBar/SearchBar.jsx
@@ -1,5 +1,5 @@
-// import searchIcon from "../../../assets/icons/Icon-search.svg"
-// import uploadIcon from "../../../assets/icons/Icon-upload.svg"
+import searchIcon from "../../../assets/icons/Icon-search.svg"
+import uploadIcon from "../../../assets/icons/Icon-upload.svg"
 import profileImage from '../../../assets/images/Mohan-muruge.jpg'
 
 import './SearchBar.scss'
@@ -9,12 +9,18 @@ const SearchBar = () => {
         <form className="search-form">
             <label htmlFor="search"></label>
             
-            <input className="search-form__input" type="text" name="search" placeholder="Search"/>
+            <div className="search-form__input-wrapper">
+                <img className="search-form__search-icon" src={searchIcon} alt="search icon"/>
+                <input className="search-form__input" type="text" name="search" placeholder="Search"/>
+            </div>
            
-            <input className="search-form__submit" type="submit" value="UPLOAD"/>
+            <button className="search-form__submit" type="submit">
+                <img className="search-form__submit-icon" src={uploadIcon} alt="upload icon"/>
+                UPLOAD
+            </button>
 
             <div className="search-form__avatar-wrapper">
-            <img className="search-form__avatar" src={profileImage} alt="Mogan muruge"/>
+                <img className="search-form__avatar" src={profileImage} alt="Mogan muruge"/>
             </div>
 
         </form>
